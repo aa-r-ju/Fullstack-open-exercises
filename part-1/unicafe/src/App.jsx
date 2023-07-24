c8e074dace3af8c947be9e385bfec4d2f47f45e3
aa-r-ju <aarju3447@gmail.com>
2023-07-24 16:37:37 +0545

    Feat:completed 1.11 exercise

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -1,15 +1,37 @@
 import { useState } from 'react'
 const StatisticLine = ({good,neutral,bad}) => {
-  console.log(good)
+  // const name = {padding:"5px"}
   return(
     <div>
     <h1> statistics</h1>
-    <p>good  {good} </p> 
-    <p>neutral  {neutral} </p>
-    <p> bad   {bad}</p>
-    <p> all {good + neutral + bad}</p>
-    <p> average {(good - bad )/ (good + neutral + bad)}</p>
-    <p> positive {(good)/ (good + neutral + bad) * 100} %</p>
+    <table>
+      <tbody>
+      <tr>
+    <td>good</td>
+    <td>{good}</td>
+  </tr>
+<tr>
+  <td>neutral</td>
+  <td> {neutral}</td>
+</tr>
+<tr> 
+  <td> bad</td>
+  <td> {bad}</td>
+</tr>
+<tr>
+<td> all</td>
+<td> {good + neutral + bad}</td>
+</tr> 
+<tr>
+  <td>average</td>
+  <td>{(good - bad )/ (good + neutral + bad )}</td>
+</tr>
+<tr> 
+  <td>positive</td>
+  <td>{(good) / (good + neutral + bad) * 100}</td>
+</tr>
+   </tbody>
+    </table>
     </div>
 
   )
