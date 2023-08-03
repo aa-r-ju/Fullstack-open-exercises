f18a44aebfe6f57ea0c927f32e29eccb07296b58
aa-r-ju <aarju3447@gmail.com>
2023-08-03 16:15:29 +0545

    feat:complete 2.9 exercise

--- part-2/courseinfo/src/App.jsx
+++ part-2/courseinfo/src/App.jsx
@@ -255,39 +255,116 @@
 
 
 
+//2.8 exercise
+// import { useState } from 'react'
 
-import { useState } from 'react'
+// const App = () => {
+//   const [persons, setPersons] = useState([
+//     { name: 'Arto Hellas', number: '123-456-7890' }
+//   ]) 
+//   const [newName, setNewName] = useState('')
+//   const [newNumber, setNewNumber] = useState('')
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const nameExists = persons.some((person) => person.name === newName);
+//     if(nameExists) {
+//       alert(`${newName} is already added to phonebook`);
+//     } else {
+//       setPersons([...persons, { name: newName, number: newNumber }])
+//       setNewName("");
+//       setNewNumber("");
+//     }
+//   }
+
+//   const handleNameChange = (event) => {
+//     setNewName(event.target.value);
+//   }
+
+//   const handleNumberChange = (event) => {
+//     setNewNumber(event.target.value);
+//   }
+
+//   return (
+//     <div>
+//       <h2>Phonebook</h2>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           name: <input type="text" value={newName} onChange={handleNameChange} />
+//         </div>
+//         <div>
+//           number: <input type="text" value={newNumber} onChange={handleNumberChange} />
+//         </div>
+//         <div>
+//           <button type="submit">add</button>
+//         </div>
+//       </form>
+//       <h2>Numbers</h2>
+//       <ul> 
+//         {persons.map((person, index) => (
+//           <li key={index}> {person.name}  {person.number}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+
+
+
+
+import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '123-456-7890' }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+  const initialPersons = [
+    { name: 'Arto Hellas', number: '123-456-7890', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ];
+
+  const [persons, setPersons] = useState(initialPersons);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const nameExists = persons.some((person) => person.name === newName);
-    if(nameExists) {
+    if (nameExists) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons([...persons, { name: newName, number: newNumber }])
-      setNewName("");
-      setNewNumber("");
+      setPersons([...persons, { name: newName, number: newNumber, id: persons.length + 1 }]);
+      setNewName('');
+      setNewNumber('');
     }
-  }
+  };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
-  }
+  };
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
-  }
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input type="text" value={searchTerm} onChange={handleSearchChange} />
+        <h2> add a new </h2>
+      </div>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input type="text" value={newName} onChange={handleNameChange} />
@@ -300,13 +377,15 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul> 
-        {persons.map((person, index) => (
-          <li key={index}> {person.name}  {person.number}</li>
+      <ul>
+        {filteredPersons.map((person, index) => (
+          <li key={index}>
+            {person.name} {person.number}
+          </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default App;
