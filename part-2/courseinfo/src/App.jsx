3f5d7255eedf13eb19206572325c9d63ca763bdd
aa-r-ju <aarju3447@gmail.com>
2023-08-03 16:01:38 +0545

    feat: completed 2.8 exercise

--- part-2/courseinfo/src/App.jsx
+++ part-2/courseinfo/src/App.jsx
@@ -101,51 +101,212 @@
 
 
 
-import { useState } from 'react';
+// import { useState } from 'react';
+
+// const App = () => {
+//   const [persons, setPersons] = useState([
+//     { name: 'Arto Hellas' },
+//   ]);
+//   const [newName, setNewName] = useState('');
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+
+//     // Check if the newName already exists in the persons array
+//     const nameExists = persons.some((person) => person.name === newName);
+
+//     if (nameExists) {
+//       alert(`${newName} already exists in the phonebook.`);
+//     } else {
+//       setPersons([...persons, { name: newName }]);
+//       setNewName('');
+//     }
+//   };
+
+//   const handleChange = (event) => {
+//     setNewName(event.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <h2>Phonebook</h2>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           name: <input type="text" value={newName} onChange={handleChange} />
+//         </div>
+//         <div>
+//           <button type="submit">add</button>
+//         </div>
+//       </form>
+//       <h2>Numbers</h2>
+//       <ul>
+//         {persons.map((person, index) => (
+//           <li key={index}> {person.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+// import { useState } from 'react'
+
+// const App = () => {
+//   const [persons, setPersons] = useState([
+//     { name: 'Arto Hellas' }
+//   ]) 
+//   const [newName, setNewName] = useState('')
+
+//   const handleSubmit = (event) =>{
+//     event.preventDefault();
+//     setPersons([...persons,{name:newName}])
+//     setNewName('');
+//   }
+
+
+//   const handleChange = (event) => {
+//     setNewName(event.target.value)
+//   }
+
+
+//   return (
+//     <div>
+//       <h2>Phonebook</h2>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           name: <input  type = "text" value = {newName}  onChange={handleChange}/>
+//         </div>
+//         <div>
+//           <button type="submit">add</button>
+//         </div>
+//       </form>
+//       <h2>Numbers</h2>
+//     <ul> 
+//       {persons.map((person,index) => (
+//         <li key = {index}> {person.name}</li>
+//       ))}
+//     </ul>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+// import { useState } from 'react'
+
+// const App = () => {
+//   const [persons, setPersons] = useState([
+//     { name: 'Arto Hellas' }
+//   ]) 
+//   const [newName, setNewName] = useState('')
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const nameExists = persons.some((person) => person.name === newName);
+//     if(nameExists) {
+//       alert(`${newName} is already added to phonebook`);
+//     } else {
+//       setPersons([...persons,{name:newName}])
+//       setNewName("");
+//     }
+//   }
+
+
+//   const handleChange = (event) => {
+//     setNewName(event.target.value)
+//   }
+
+//   return (
+//     <div>
+//       <h2>Phonebook</h2>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           name: <input type = "text" value = {newName} onChange={handleChange} />
+//         </div>
+//         <div>
+//           <button type="submit">add</button>
+//         </div>
+//       </form>
+//       <h2>Numbers</h2>
+//     <ul> 
+//       {persons.map((person,index) => (
+//         <li key = {index}> {person.name}</li>
+//       ))}
+//     </ul>
+//     </div>
+//   )
+// }
+
+// export default App 
+
+
+
+
+
+
+
+
+
+
+import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
-  ]);
-  const [newName, setNewName] = useState('');
+    { name: 'Arto Hellas', number: '123-456-7890' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    // Check if the newName already exists in the persons array
     const nameExists = persons.some((person) => person.name === newName);
-
-    if (nameExists) {
-      alert(`${newName} already exists in the phonebook.`);
+    if(nameExists) {
+      alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons([...persons, { name: newName }]);
-      setNewName('');
+      setPersons([...persons, { name: newName, number: newNumber }])
+      setNewName("");
+      setNewNumber("");
     }
-  };
+  }
 
-  const handleChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value);
-  };
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input type="text" value={newName} onChange={handleChange} />
+          name: <input type="text" value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input type="text" value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
+      <ul> 
         {persons.map((person, index) => (
-          <li key={index}> {person.name}</li>
+          <li key={index}> {person.name}  {person.number}</li>
         ))}
       </ul>
     </div>
-  );
-};
+  )
+}
 
 export default App;
